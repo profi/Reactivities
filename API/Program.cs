6ecf297b53a2266a6fa18d53ea3dba69916169a7
Kliment Petreski <Kliment.Petreski@damilah.com>
2023-09-09 15:04:42 +0200

    S3-CORS. Semanntic-UI

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("AllowSpecificOrigin",
+            builder => builder.WithOrigins("http://127.0.0.1:5173")
+                              .AllowAnyMethod()
+                              .AllowAnyHeader());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 
---
+++ client-app/src/demo.ts
@@ -0,0 +1,1 @@
+const data : number = 42;
